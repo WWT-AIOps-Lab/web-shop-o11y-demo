6430659912ad28cc2a40fb3542a2e43d57047a1a
root <root@webshop.aiops.wwtatc.local>
2023-04-28 11:05:50 +0100

    Adding new script to prep the server

---
+++ prep-os.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Check for operating system type
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  # Check for Linux distribution/flavor
+  if [ -f /etc/os-release ]; then
+      . /etc/os-release
+      echo "This is a $NAME ($VERSION) system"
+      if [[ "$NAME" == "CentOS Linux" ]]; then
+        echo "This is a CentOS system"
+        yum -y install git-all
+        git --version
+        yum install -y yum-utils
+        yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+        yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+        systemctl start docker
+        docker --version
+        docker run hello-world
+      fi
+  elif [ -f /etc/lsb-release ]; then
+      . /etc/lsb-release
+      echo "This is a $DISTRIB_ID ($DISTRIB_RELEASE) system"
+  elif [ -f /etc/redhat-release ]; then
+      echo "This is a $(cat /etc/redhat-release) system"
+      if grep -q "CentOS" /etc/redhat-release; then
+        echo "This is a CentOS system"
+      fi
+  else
+      echo "Unable to determine Linux distribution/flavor"
+  fi
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+  echo "This is a macOS system"
+elif [[ "$OSTYPE" == "cygwin" ]]; then
+  echo "This is a Cygwin system"
+elif [[ "$OSTYPE" == "msys" ]]; then
+  echo "This is a Windows MSYS system"
+elif [[ "$OSTYPE" == "win32" ]]; then
+  echo "This is a Windows system"
+else
+  echo "This operating system is not supported"
+fi
+
